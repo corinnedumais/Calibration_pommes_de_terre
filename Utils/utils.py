1c808292f6ef7f5bee9bb21b60c9c49076df893f
corinnedumais <63109710+corinnedumais@users.noreply.github.com>
2022-05-13 09:56:42 -0400

    Added documentation and type-hinting

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,23 +1,47 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 
 
-def pixels_to_mm(px_measure, original_resolution, resize_factor):
+def pixels_to_mm(px_measure: float, original_resolution: int, resize_factor: float) -> float:
     """
-    Function to convert a the length of pixel segment to mm.
+    Function to convert a the length of pixel segment to millimeters (mm).
 
-    Since 1po = 25.4mm, conversion is done using resolution in ppi, with a correction to account for resizing.
+    Since 1 inch = 25.4 mm, conversion is done using resolution in ppi, with a correction to account for resizing.
 
-    Parameters:
-        px_measure (int or float): Measure to convert to mm
-        original_resolution (int): Resolution (horizontal and vertical) of original image in ppi (pixels per inches)
-        resize_factor (int or float): Factor by which the image was resized
+    Parameters
+    ----------
+    px_measure: float
+                Measure to convert to mm.
+    original_resolution: int
+                Resolution (horizontal and vertical) of original image in ppi (pixels per inches)
+    resize_factor: float
+                Factor by which the image was resized.
+
+    Returns
+    -------
+    float
+          Measure in millimeters.
     """
     return px_measure * 25.4/original_resolution * resize_factor
 
 
-def normalize_dataset(dataset):
+def normalize_dataset(dataset: np.ndarray) -> np.ndarray:
+    """
+    Function to normalize each attribut of a dataset. Minimum value will be 0 and maximum value will be 1.
+
+    Parameters
+    ----------
+    dataset: np.ndarray
+             Dataset to normalize. Should be shape (n, m) where n is number of samples and m is the number of features.
+
+    Returns
+    -------
+    np.ndarray
+               Dataset with each attribut individually normalized.
+    """
     # Find the min and max values for each column
     min_max = []
+
     for i in range(len(dataset[0])):
         col_values = dataset.T[i]
         min_max.append((min(col_values), max(col_values)))
@@ -25,4 +49,5 @@ def normalize_dataset(dataset):
     for row in dataset:
         for i in range(len(row)):
             row[i] = (row[i] - min_max[i][0]) / (min_max[i][1] - min_max[i][0])
+
     return dataset
