d7c93b8665d5bf14d4fbde85fd096aa914a16654
corinnedumais <63109710+corinnedumais@users.noreply.github.com>
2022-05-13 09:29:48 -0400

    Cleaned-up code

--- predict_weight.py
+++ predict_weight.py
@@ -1,11 +1,10 @@
-import keras
-import numpy as np
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 from scipy.stats import linregress
-
-from tensorflow.keras import utils, layers
 from tensorflow import keras
+from tensorflow.keras import layers
 
 from Utils.weights import normalize_dataset
 
@@ -13,7 +12,8 @@ np.random.seed(6)
 
 # Define parameters
 train_ratio = 0.7
-variety = 'mountain_gem'  # must be 'burbank' of 'mountain_gem'
+variety = 'mountain_gem'
+assert variety in ['burbank', 'mountain_gem']
 
 # Load dataset and normalize data
 dataset = np.loadtxt(f'SolanumTuberosum/Dimensions/{variety}.txt', skiprows=2)
@@ -43,7 +43,8 @@ callbacks = [keras.callbacks.ModelCheckpoint(f"Trained Models/{variety}_weight.h
 
 # Train the model
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.001))
-model.fit(train, train_labels, epochs=300, validation_data=(test, test_labels), batch_size=4, shuffle=True, callbacks=callbacks)
+model.fit(train, train_labels, epochs=300, validation_data=(test, test_labels), batch_size=4, shuffle=True,
+          callbacks=callbacks)
 
 # Load the model and evaluate
 model = keras.models.load_model(f"Trained Models/{variety}_weight.h5")
@@ -65,14 +66,21 @@ res_test = linregress(test_labels, [i[0] for i in preds_test])
 
 # Display results
 fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 5))
+
+# Scatter plots for all (true, pred) points
 ax1.scatter(train_labels, [i[0] for i in preds_train])
-ax1.plot(train_labels, [res_train.slope * i + res_train.intercept for i in train_labels], label='Régression linéaire\n'
-                                                                                                f'y={res_train.slope:.2f}x+{res_train.intercept:.2f}\n'
-                                                                                                f'R$^2$={res_train.rvalue ** 2:.4f}')
 ax2.scatter(test_labels, [i[0] for i in preds_test], c='red')
-ax2.plot(test_labels, [res_test.slope * i + res_test.intercept for i in test_labels], 'red', label='Régression linéaire\n'
-                                                                                                f'y={res_test.slope:.2f}x+{res_test.intercept:.2f}\n'
-                                                                                                f'R$^2$={res_test.rvalue ** 2:.4f}')
+
+# Plot linear regression results
+ax1.plot(train_labels, [res_train.slope * i + res_train.intercept for i in train_labels],
+         label='Régression linéaire\n'
+         f'y={res_train.slope:.2f}x+{res_train.intercept:.2f}\n'
+         f'R$^2$={res_train.rvalue ** 2:.4f}')
+ax2.plot(test_labels, [res_test.slope * i + res_test.intercept for i in test_labels], 'red',
+         label='Régression linéaire\n'
+         f'y={res_test.slope:.2f}x+{res_test.intercept:.2f}\n'
+         f'R$^2$={res_test.rvalue ** 2:.4f}')
+
 ax1.set_title("Données d'entraînement")
 ax1.set_xlabel('Poids réel [g]')
 ax1.set_ylabel('Poids prédit [g]')
