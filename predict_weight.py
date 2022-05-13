50e382fbb8ef97a14a71564a0c7bb6565cc0ed8e
corinnedumais <63109710+corinnedumais@users.noreply.github.com>
2022-05-13 09:24:29 -0400

    Cleaned-up code

--- predict_weight.py
+++ predict_weight.py
@@ -10,11 +10,13 @@ from tensorflow import keras
 from Utils.weights import normalize_dataset
 
 np.random.seed(6)
+
+# Define parameters
 train_ratio = 0.7
 variety = 'mountain_gem'  # must be 'burbank' of 'mountain_gem'
 
+# Load dataset and normalize data
 dataset = np.loadtxt(f'SolanumTuberosum/Dimensions/{variety}.txt', skiprows=2)
-
 data, labels = normalize_dataset(dataset.T[:2].T), dataset.T[2]
 
 # Shuffle data and target
@@ -22,10 +24,12 @@ perm = np.random.permutation(len(data))
 data = data[perm]
 labels = labels[perm]
 
+# Split train/test samples
 n = int(train_ratio * len(data))
 train, train_labels = data[:n], labels[:n]
 test, test_labels = data[n:], labels[n:]
 
+# Define model
 model = keras.Sequential([
     layers.Dense(128, activation='relu'),
     layers.GaussianNoise(0.01),
@@ -37,14 +41,16 @@ model = keras.Sequential([
 
 callbacks = [keras.callbacks.ModelCheckpoint(f"Trained Models/{variety}_weight.h5", save_best_only=True)]
 
+# Train the model
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.001))
 model.fit(train, train_labels, epochs=300, validation_data=(test, test_labels), batch_size=4, shuffle=True, callbacks=callbacks)
 
+# Load the model and evaluate
 model = keras.models.load_model(f"Trained Models/{variety}_weight.h5")
-
 preds_train = model.predict(train)
 preds_test = model.predict(test)
 
+# Print expected values vs predictions
 print('\nTRAIN EVALUATION')
 for label, pred in zip(train_labels, preds_train):
     print(f'Expected value: {label:.1f} g      Predicted value: {pred[0]:.1f} g')
@@ -53,11 +59,11 @@ print('\nTEST EVALUATION')
 for label, pred in zip(test_labels, preds_test):
     print(f'Expected value: {label:.1f} g      Predicted value: {pred[0]:.1f} g')
 
+# Linear regression
 res_train = linregress(train_labels, [i[0] for i in preds_train])
-print(res_train.slope, res_train.rvalue ** 2)
 res_test = linregress(test_labels, [i[0] for i in preds_test])
-print(res_test.slope, res_test.rvalue ** 2)
 
+# Display results
 fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 5))
 ax1.scatter(train_labels, [i[0] for i in preds_train])
 ax1.plot(train_labels, [res_train.slope * i + res_train.intercept for i in train_labels], label='Régression linéaire\n'
@@ -68,13 +74,15 @@ ax2.plot(test_labels, [res_test.slope * i + res_test.intercept for i in test_lab
                                                                                                 f'y={res_test.slope:.2f}x+{res_test.intercept:.2f}\n'
                                                                                                 f'R$^2$={res_test.rvalue ** 2:.4f}')
 ax1.set_title("Données d'entraînement")
-ax2.set_title("Données de test")
 ax1.set_xlabel('Poids réel [g]')
 ax1.set_ylabel('Poids prédit [g]')
+ax1.legend()
+
+ax2.set_title("Données de test")
 ax2.set_xlabel('Poids réel [g]')
 ax2.set_ylabel('Poids prédit [g]')
-ax1.legend()
 ax2.legend()
+
 plt.tight_layout()
 plt.savefig(f'{variety}.png', dpi=500)
 plt.show()
