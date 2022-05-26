6f4e47d6cbb82b67247bfec4479892180375825a
corinnedumais <63109710+corinnedumais@users.noreply.github.com>
2022-05-26 10:51:02 -0400

    to get metadata

--- position.py
+++ position.py
@@ -1,6 +1,7 @@
 import exifread
 
-with open('IMG_3732.jpg', 'rb') as f:
+with open('IMG_3729.jpg', 'rb') as f:
     exif_dict = exifread.process_file(f)
     for key in exif_dict:
-        print("%s: %s" % (key, exif_dict[key]))
+        if 'MakerNote' not in key:
+            print(f"{key}: {exif_dict[key]}")
