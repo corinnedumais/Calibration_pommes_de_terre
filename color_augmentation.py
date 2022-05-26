6c0a50afae78a10207bc41d564768c3ae6ef94f2
corinnedumais <63109710+corinnedumais@users.noreply.github.com>
2022-05-26 10:49:57 -0400

    script for changing background color

---
+++ color_augmentation.py
@@ -0,0 +1,27 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv2.imread('SolanumTuberosum/Images/01.jpg')
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# define range of blue color in HSV
+lower_brown = np.array([0, 80, 20])
+upper_brown = np.array([30, 255, 255])
+
+# Threshold the HSV image to get only blue colors
+mask = cv2.inRange(hsv, lower_brown, upper_brown)
+
+# Bitwise-AND mask and original image
+res = cv2.bitwise_and(img, img, mask=mask)
+
+# res = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
+res[np.where((res == [0, 0, 0]).all(axis=2))] = [255, 0, 0]
+
+plt.imshow(res, cmap='gray')
+plt.axis('off')
+plt.tight_layout()
+plt.show()
+
+cv2.imwrite('01_black.jpg', res)
