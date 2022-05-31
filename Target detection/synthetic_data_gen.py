c713ee538f5136d03cdedd477a114ba4b3bf9030
corinnedumais <63109710+corinnedumais@users.noreply.github.com>
2022-05-30 20:51:14 -0400

    reorganized code

--- Target detection/synthetic_data_gen.py
+++ Target detection/synthetic_data_gen.py
@@ -3,59 +3,71 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse, Rectangle
 from tqdm import tqdm
 
+browns = [(225 / 255, 193 / 255, 110 / 255), (218 / 255, 160 / 255, 109 / 255), (222 / 255, 184 / 255, 135 / 255),
+          (225 / 255, 193 / 255, 110 / 255), (193 / 255, 154 / 255, 107 / 255)]
 
-def too_much_overlap(center, centers_list):
+
+def too_much_overlap(center, centers_list, max_d):
     x1, y1 = center
     for c in centers_list:
         x2, y2 = c
         d = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-        if d < 100:
+        if d < max_d:
             return True
     return False
 
 
-browns = [(225 / 255, 193 / 255, 110 / 255), (218 / 255, 160 / 255, 109 / 255), (222 / 255, 184 / 255, 135 / 255),
-          (225 / 255, 193 / 255, 110 / 255), (193 / 255, 154 / 255, 107 / 255)]
-
-for it in tqdm(range(38, 41)):
+def generate_background(noise):
     main_color = np.array(np.random.choice(range(256), size=3))
     img = np.zeros((1536, 2048, 3), dtype=np.uint8)
-
     for i in range(1536):
         for j in range(2048):
-            if np.random.rand() > 0.12:
+            if np.random.rand() > noise:
                 img[i, j, :] = main_color
             else:
                 random_color = np.array(np.random.choice(range(256), size=3))
                 img[i, j, :] = random_color
+    return img
 
-    num_pdt = np.random.randint(30, 60)
-    num_cible = 6
 
-    fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
-    fig2, ax2 = plt.subplots(subplot_kw={'aspect': 'equal'})
-    ax2.imshow(np.zeros((1536, 2048)), cmap='gray')
+def generate_synthetic_img(no, noise=0.1, instances=(30, 60), targets=6, x_pos=range(500, 1600), y_pos=range(400, 1000),
+                           widths=range(40, 100), heights=range(50, 180), max_d=100):
+    # Generate background of random color with noise
+    img = generate_background(noise)
 
+    # Random number of PDT within given range
+    num_pdt = np.random.randint(instances[0], instances[1] + 1)
+
+    # List to keep track of center positions of objects in the image
     center_coords = []
 
-    for i in range(num_cible):
-        size = np.random.randint(30, 80)
+    # Initialize plots
+    fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})    # For images
+    fig2, ax2 = plt.subplots(subplot_kw={'aspect': 'equal'})  # For masks
+
+    # Draw targets
+    for i in range(targets):
+        size = np.random.randint(40, 90)
         angle = np.random.random() * 90
         xo, yo = np.random.choice(range(300, 1700)), np.random.choice(range(250, 1250))
 
-        while too_much_overlap((xo, yo), center_coords):
+        # Find center coordinates that dont cause too much overlap with already drawn targets
+        while too_much_overlap((xo, yo), center_coords, max_d):
             xo, yo = np.random.choice(range(300, 1700)), np.random.choice(range(250, 1250))
 
+        # Bigger white square part of the target
         r = Rectangle((xo, yo), size, size, angle=angle)
         ax.add_artist(r)
         r.set_clip_box(ax.bbox)
         r.set_facecolor((1, 1, 1))
 
+        # Mask
         r2 = Rectangle((xo, yo), size, size, angle=angle)
         ax2.add_artist(r2)
         r2.set_clip_box(ax2.bbox)
         r2.set_facecolor((1, 1, 1))
 
+        # Two smaller diagonal black squares part of the target
         r = Rectangle((xo, yo), size / 2, size / 2, angle=angle)
         ax.add_artist(r)
         r.set_clip_box(ax.bbox)
@@ -68,29 +80,63 @@ for it in tqdm(range(38, 41)):
         r.set_clip_box(ax.bbox)
         r.set_facecolor((0, 0, 0))
 
+    # Once all the targets are drawn, save the masks
+    ax2.imshow(np.zeros((1536, 2048)), cmap='gray')
     ax2.invert_yaxis()
     fig2.gca().set_axis_off()
     fig2.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
     plt.margins(0, 0)
-    fig2.savefig(f'Synthetic_masks/syn_mask{it:03}.jpg', dpi=500)
+    fig2.savefig(f'Synthetic_masks/syn_mask{no:02}.png', dpi=500)
 
-    ells = []
+    # Draw potatoes
     for i in range(num_pdt):
-        xy = (np.random.choice(range(500, 1600)), np.random.choice(range(400, 1000)))
-        while too_much_overlap(xy, center_coords):
-            xy = (np.random.choice(range(500, 1600)), np.random.choice(range(400, 1000)))
-        e = Ellipse(xy=xy, width=np.random.choice(range(40, 100)), height=np.random.choice(range(50, 180)),
+        xy = (np.random.choice(x_pos), np.random.choice(y_pos))
+        while too_much_overlap(xy, center_coords, max_d):
+            xy = (np.random.choice(x_pos), np.random.choice(y_pos))
+        e = Ellipse(xy=xy, width=np.random.choice(widths), height=np.random.choice(heights),
                     angle=np.random.rand() * 360)
         center_coords.append(xy)
         ax.add_artist(e)
         e.set_clip_box(ax.bbox)
         e.set_facecolor(browns[np.random.randint(0, len(browns))])
 
+    # Once all the instances are drawn, save the image
     ax.set_xlim(0, 2048)
     ax.set_ylim(0, 1536)
-
     ax.imshow(img)
     fig.gca().set_axis_off()
     fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
     plt.margins(0, 0)
-    fig.savefig(f'Synthetic_data/syn{it:03}.jpg', dpi=500)
+    fig.savefig(f'Synthetic_data/syn{no:02}.jpg', dpi=500)
+
+
+for it in tqdm(range(1, 6)):
+    generate_synthetic_img(it, noise=0.05, instances=(40, 50), targets=6, x_pos=range(700, 1500), y_pos=range(500, 900),
+                           widths=range(60, 100), heights=range(70, 180), max_d=60)
+# for it in tqdm(range(6, 11)):
+#     generate_synthetic_img(it, noise=0.06, instances=(50, 60), targets=6, x_pos=range(400, 1600), y_pos=range(400, 1300),
+#                            widths=range(80, 110), heights=range(100, 180), max_d=75)
+# for it in tqdm(range(11, 16)):
+#     generate_synthetic_img(it, noise=0.07, instances=(70, 80), targets=7, x_pos=range(400, 1700), y_pos=range(200, 1000),
+#                            widths=range(90, 100), heights=range(130, 160), max_d=70)
+# for it in tqdm(range(16, 21)):
+#     generate_synthetic_img(it, noise=0.08, instances=(60, 70), targets=5, x_pos=range(500, 1500), y_pos=range(600, 1100),
+#                            widths=range(80, 90), heights=range(150, 180), max_d=60)
+# for it in tqdm(range(21, 26)):
+#     generate_synthetic_img(it, noise=0.09, instances=(90, 100), targets=6, x_pos=range(200, 1900), y_pos=range(200, 1400),
+#                            widths=range(70, 110), heights=range(110, 180), max_d=60)
+# for it in tqdm(range(26, 31)):
+#     generate_synthetic_img(it, noise=0.15, instances=(80, 90), targets=8, x_pos=range(200, 1800), y_pos=range(300, 1300),
+#                            widths=range(90, 120), heights=range(130, 170), max_d=70)
+# for it in tqdm(range(31, 36)):
+#     generate_synthetic_img(it, noise=0.14, instances=(40, 50), targets=6, x_pos=range(400, 1700), y_pos=range(400, 1200),
+#                            widths=range(100, 130), heights=range(160, 200), max_d=100)
+# for it in tqdm(range(36, 41)):
+#     generate_synthetic_img(it, noise=0.13, instances=(60, 70), targets=6, x_pos=range(400, 1700), y_pos=range(400, 1000),
+#                            widths=range(60, 100), heights=range(70, 180), max_d=70)
+# for it in tqdm(range(41, 46)):
+#     generate_synthetic_img(it, noise=0.12, instances=(70, 80), targets=6, x_pos=range(400, 1700), y_pos=range(400, 1000),
+#                            widths=range(60, 100), heights=range(70, 180), max_d=90)
+# for it in tqdm(range(46, 51)):
+#     generate_synthetic_img(it, noise=0.12, instances=(80, 90), targets=6, x_pos=range(400, 1700), y_pos=range(400, 1000),
+#                            widths=range(60, 100), heights=range(70, 180), max_d=50)
